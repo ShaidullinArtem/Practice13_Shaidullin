070df69d405b2bf3ea26c6816e257e6e6bd9463d
MaksimovK <anderjblinov35@gmail.comm>
2023-03-13 13:27:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/practice13_shaidullin/OnboardingScreen.kt
@@ -0,0 +1,11 @@
+package com.example.practice13_shaidullin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
